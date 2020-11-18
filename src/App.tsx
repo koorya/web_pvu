e5b92ff42291fca17dbab43e5207642576c2a18b
Kirill Ivanov <puaktopblm@yandex.ru>
2020-11-18 23:50:09 +0700

    сделал проверку валидности ввода перед записью

--- src/App.tsx
+++ src/App.tsx
@@ -7,57 +7,72 @@ import { iPlcVar } from "./components/iPlcVar";
 interface iProps {}
 interface iState {
   plc_vars: iPlcVar[];
-  user_values: iPlcVar[];
 }
-var i = 0;
+const plc_variables: iPlcVar[] = [
+  {
+    id: 1,
+    type: "int",
+    name: "state1",
+    value: 0,
+  },
+  {
+    id: 2,
+    type: "float",
+    name: "x",
+    value: 2.4,
+  },
+  {
+    id: 3,
+    type: "bool",
+    name: "enable",
+    value: true,
+  },
+];
 function getPlcVariables(): iPlcVar[] {
-  return [
-    {
-      id: 1,
-      type: "int",
-      name: "state1",
-      value: i++,
-    },
-    {
-      id: 2,
-      type: "float",
-      name: "x",
-      value: 2.4,
-    },
-    {
-      id: 3,
-      type: "bool",
-      name: "enable",
-      value: true,
-    },
-  ]
+  return plc_variables;
 }
 
 function getState(): iState {
   const ret: iState = {
     plc_vars: getPlcVariables(),
-    user_values: [],
   };
-  ret.user_values = ret.plc_vars.map((item) => Object.assign({}, item));
   return ret;
 }
 
 class App extends Component<iProps, iState> {
   state = getState();
-
+  timerID: NodeJS.Timeout;
+  constructor(props: iProps) {
+    super(props);
+    this.timerID = setTimeout(() => {}, 0);
+  }
   componentDidMount() {
-    setInterval(()=>{this.setState({plc_vars: getPlcVariables()})}, 1000);
+    this.timerID = setInterval(() => {
+      plc_variables[0].value++;
+      this.setState({ plc_vars: getPlcVariables() });
+    }, 1000);
   }
 
-  value_changed = (id: number, value: any) => {
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  value_changed = (item: iPlcVar, value: any) => {
     // this.state.user_values.filter((u_v) => u_v.id === id)[0].value = value;
     // this.setState({user_values: this.state.user_values});
   };
-  
-  resetValue = (id: number) => {
 
-  }
-  
+  resetValue = (id: number) => {
+    console.log(`reset value ${id}`);
+  };
+  writeValue = (id: number, value: any) => {
+    const _state = getPlcVariables();
+    const item = _state.find((item) => item.id === id);
+    if (item) item.value = value;
+    this.setState({ plc_vars: _state });
+    return true;
+  };
+
   render() {
     return (
       <div className="App">
@@ -65,13 +80,9 @@ class App extends Component<iProps, iState> {
           <VarItem
             key={item.id}
             varitem={item}
-            user_value= {item.value}
-            // {
-            //   this.state.user_values.filter((u_v) => u_v.id === item.id)[0]
-            //     .value
-            // }
             value_change={this.value_changed}
             reset={this.resetValue}
+            writeValue={this.writeValue}
           />
         ))}
       </div>
--- src/components/VarItem.tsx
+++ src/components/VarItem.tsx
@@ -1,69 +1,101 @@
 import React, { Component } from "react";
-import { iPlcVar } from './iPlcVar';
+import { iPlcVar } from "./iPlcVar";
 
 interface iProps {
-	key: any;
-  varitem: any;
-	user_value: any;
-	value_change: Function;
-	reset: (id: number) => void;
+  key: any;
+  varitem: iPlcVar;
+  value_change: (item: iPlcVar, value: any) => void;
+  reset: (id: number) => void;
+  writeValue: (id: number, value: any) => boolean;
 }
 interface iState {
-	user_value: any;
-	value_class_name: string
+  user_value: any;
+  validity: any;
+  value_class_name: string;
 }
 
 export default class VarItem extends Component<iProps, iState> {
-	state = {
-		value_class_name: "varvalue",
-		user_value: "any"
-	}
-
+  input: any;
+  state = {
+    value_class_name: "varvalue",
+    validity: true,
+    user_value: "any",
+  };
+  constructor(props: iProps) {
+    super(props);
+    this.input = React.createRef();
+  }
 
   onChange = (e: { target: { value: any; name: any } }) => {
     const new_value = e.target.value;
-	
-		this.setState({ 'user_value': new_value });
-		this.props.value_change(this.props.varitem.id, new_value);
-	
-		if (this.props.varitem.value.toString() !== new_value.toString())
-      this.setState({ value_class_name: "varvalue different" });
-		else 
-			this.setState({ value_class_name: "varvalue" });
 
-	};
-	
-	componentDidMount() {
-		this.setState({user_value: this.props.user_value});
-	}
+    this.setState({ user_value: new_value });
+    this.props.value_change(this.props.varitem, new_value);
+
+    if (this.props.varitem.value.toString() !== new_value.toString())
+      this.setState({ value_class_name: "varvalue different" });
+    else this.setState({ value_class_name: "varvalue" });
+  };
+
+  componentDidMount() {
+    this.setState({ user_value: this.props.varitem.value });
+  }
+
+  resetValue = () => {
+    this.setState({ user_value: this.props.varitem.value });
+    this.setState({ value_class_name: "varvalue" });
+    this.props.reset(this.props.varitem.id);
+  };
+
+  writeValue = () => {
+    const write_val = new Promise((resolve, reject) => {
+      if (
+        this.input.current.reportValidity() &&
+        this.props.writeValue(this.props.varitem.id, this.state.user_value)
+      ) {
+        resolve();
+      }
+    });
+    write_val.then(() => {
+      this.onChange({ target: { value: this.props.varitem.value, name: "" } });
+      console.log(`value updated ${this.props.varitem.value}`);
+    });
+  };
 
   render() {
     return (
-			<div style={{"padding": "1px"}}>
-				<div className="varitem">
-					<div className="varitem_name" >
-						{this.props.varitem.name}
-					</div>
+      <div style={{ padding: "1px" }}>
+        <div className="varitem">
+          <div className="varitem_name">{this.props.varitem.name}</div>
 
-					<div className="varitem_plcvalue" >
-						{this.props.varitem.value.toString()}
-					</div>
-					<div className="varitem_uservalue">				
-						<input 
-						
-							type="text"
-							name="user_value"
-							value={this.state.user_value}
-							className={this.state.value_class_name}
-							onChange={this.onChange}
-							pattern={getPattern(this.props.varitem.type)}
-						/>
-						<input type="button" value="x" name="reset" onClick={(e) => this.props.reset(this.props.varitem.id)}/>
-						<input type="button" value="write"/>
-					</div>
-				</div>
-				<div style={{"clear": "both"}}></div>
-			</div>
+          <div className="varitem_plcvalue">
+            {this.props.varitem.value.toString()}
+          </div>
+          <div className="varitem_uservalue">
+            <input
+              ref={this.input}
+              type="text"
+              name="user_value"
+              value={this.state.user_value}
+              className={this.state.value_class_name}
+              onChange={this.onChange}
+              pattern={getPattern(this.props.varitem.type)}
+            />
+            <input
+              type="button"
+              value="x"
+              name="reset"
+              onClick={(e) => this.resetValue()}
+            />
+            <input
+              type="button"
+              value="write"
+              onClick={(e) => this.writeValue()}
+            />
+          </div>
+        </div>
+        <div style={{ clear: "both" }}></div>
+      </div>
     );
   }
 }
