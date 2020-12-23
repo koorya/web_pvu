093132bc444a5e90c8635d7ad1bf916b3aab764d
Kirill Ivanov <puaktopblm@yandex.ru>
2020-12-23 14:29:21 +0700

    изображение загружается по кнопке

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,22 +1,24 @@
 import React, { Component } from "react";
-import Axios from "axios";
+// import Axios from "axios";
 import parse from "url-parse";
 
 interface iProps {}
 interface iState {
+  image_showed: boolean;
 	image_key: any;
 	imagesrc: string;
 }
 
 export default class Image extends Component<iProps, iState> {
   state = {
+    image_showed: false,
 		image_key: 1,
 		imagesrc: `http://${parse(window.location.href).hostname}:5000/image.png0`,
   };
   componentDidMount() {    
   }
   updateImage = () => {
-		this.setState({image_key: this.state.image_key+1, imagesrc: `http://${parse(window.location.href).hostname}:5000/image.png${this.state.image_key}`});
+		this.setState({image_showed: true, image_key: this.state.image_key+1, imagesrc: `http://${parse(window.location.href).hostname}:5000/image.png${this.state.image_key}`});
   };
 
   render() {
@@ -27,12 +29,13 @@ export default class Image extends Component<iProps, iState> {
           value={`update image`}
           onClick={(e) => this.updateImage()}
         />
-        <img
-					key={this.state.image_key}
-          src={this.state.imagesrc}
-          alt="cam vid"
-          width="100%"
-        />
+        <div className="ImageDiv">
+          <img
+            key={this.state.image_key}
+            src={(this.state.image_showed ? this.state.imagesrc : "")}
+            alt="cam vid"
+          />
+        </div>
       </div>
     );
   }
